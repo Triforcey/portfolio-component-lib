48707f83b6e7f07632f0822a88cba12326feaca5
JD Flamm <judsonwebdesign@gmail.com>
2025-10-24 23:13:03 -0600

    working storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,28 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import path from 'path';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: [
+    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'
   ],
-  "addons": [],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
+  addons: [],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {}
+  },
+  core: {
+    builder: '@storybook/builder-vite'
+  },
+  async viteFinal(config) {
+    const { mergeConfig } = await import('vite');
+    return mergeConfig(config, {
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, '../src'),
+        },
+      }
+    });
+  },
 };
+
 export default config;
